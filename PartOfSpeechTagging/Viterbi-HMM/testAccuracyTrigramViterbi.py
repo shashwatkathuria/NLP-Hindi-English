e16e392619e2a6621076125233adcd3a9cd1b0a4
Shashwat Kathuria <shashwatkathuria@yahoo.co.in>
2019-04-18 15:59:50 +0530

    Slightly modified for including various lambdas.

--- PartOfSpeechTagging/Viterbi-HMM/testAccuracyTrigramViterbi.py
+++ PartOfSpeechTagging/Viterbi-HMM/testAccuracyTrigramViterbi.py
@@ -1,27 +1,30 @@
 # -*- coding: utf-8 -*-
 
 from __future__ import division
-import codecs
+import codecs, time
 from collections import defaultdict
 from trigramViterbi import trigramHMMViterbiAlgorithm, calculateTransitionProbabilities, calculateEmissionProbabilities
 
 
 def main():
+    Lambdas = [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
 
-    # Opening the input training file
-    trainFile = codecs.open("trainDataHindi.txt", mode = "r", encoding = "utf-8")
-    # Getting the emission probabilities
-    emissionProbabilityDict = calculateEmissionProbabilities(trainFile)
-    # Opening the input training file
-    trainFile = codecs.open("trainDataHindi.txt", mode = "r", encoding = "utf-8")
-    # Getting the transition probabilities and all the tags (states)
-    transitionProbabilityDict, allTags = calculateTransitionProbabilities(trainFile)
+    for Lambda in Lambdas:
+        # Opening the input training file
+        trainFile = codecs.open("trainDataHindi.txt", mode = "r", encoding = "utf-8")
+        # Getting the emission probabilities
+        emissionProbabilityDict = calculateEmissionProbabilities(trainFile, Lambda)
+        # Opening the input training file
+        trainFile = codecs.open("trainDataHindi.txt", mode = "r", encoding = "utf-8")
+        # Getting the transition probabilities and all the tags (states)
+        transitionProbabilityDict, allTags = calculateTransitionProbabilities(trainFile, Lambda)
 
-    # Opening the input test file
-    testFile = codecs.open("testDataHindi.txt", mode = "r", encoding = "utf-8")
-    # Calling the test function to get the accuracy of algorithm on the test data file
-    testTrigramHMMViterbiAlgorithm(testFile, allTags, emissionProbabilityDict, transitionProbabilityDict)
-
+        # Opening the input test file
+        testFile = codecs.open("testDataHindi.txt", mode = "r", encoding = "utf-8")
+        # Calling the test function to get the accuracy of algorithm on the test data file
+        accuracy = testTrigramHMMViterbiAlgorithm(testFile, allTags, emissionProbabilityDict, transitionProbabilityDict)
+        print(accuracy)
+        time.sleep(5)
 
 def testTrigramHMMViterbiAlgorithm(testFile, allTags, emissionProbabilityDict, transitionProbabilityDict):
 
@@ -68,16 +71,12 @@ def testTrigramHMMViterbiAlgorithm(testFile, allTags, emissionProbabilityDict, t
             except:
                 print("Couldn't Tag Sentence")
                 continue
+            if total > 1000:
+                return correct/total
             # print("\n\nPREDICTED:\n\n")
             # print(predicted)
 
-
-
-            # assert False
-
-
-
-
+    return correct/total
 
 if __name__ == "__main__":
     main()
