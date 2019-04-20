4b3530d9edc9eada3f47f25f88494f2a273edd05
Shashwat Kathuria <shashwatkathuria@yahoo.co.in>
2019-04-20 11:35:05 +0530

    Added comments and some indentation.

--- PartOfSpeechTagging/Viterbi-HMM/bigramViterbi.py
+++ PartOfSpeechTagging/Viterbi-HMM/bigramViterbi.py
@@ -5,13 +5,26 @@ import codecs
 from collections import defaultdict
 
 def main():
+
+    # Opening the input training file
     trainFile = codecs.open("trainDataHindi.txt", mode = "r", encoding = "utf-8")
-    emissionProbabilityDict = calculateEmissionProbabilities(trainFile)
+
+    # Getting the emission probabilities
+    emissionProbabilityDict = calculateEmissionProbabilities(trainFile, 0.0)
+
+    # Opening the input training file
     trainFile = codecs.open("trainDataHindi.txt", mode = "r", encoding = "utf-8")
-    transitionProbabilityDict, allTags = calculateTransitionProbabilities(trainFile)
+
+    # Getting the transition probabilities and all the tags (states)
+    transitionProbabilityDict, allTags = calculateTransitionProbabilities(trainFile, 0.0)
+
+    # Opening the input file
     inputSentences = codecs.open("input.txt", mode = "r", encoding = "utf-8")
 
+    # Initializing a list for sentences
     sentencesList = []
+
+    # Reading the input file and storing the sentences
     for line in inputSentences:
         sentence = []
         tokens = line.split()
@@ -21,78 +34,98 @@ def main():
 
         sentencesList.append(sentence)
 
+    print("\n===========================\nPRINTING THE RESULTS\n===========================\n")
+    # Iterating through each of the sentences
     for sentence in sentencesList:
-        x = bigramHMMViterbiAlgorithm(sentence, allTags, emissionProbabilityDict, transitionProbabilityDict)
-        print(x)
+
+        # Calling the algorithm on the sentence
+        predictedWordsAndTags = bigramHMMViterbiAlgorithm(sentence, allTags, emissionProbabilityDict, transitionProbabilityDict)
+        print("\n=======\n")
+        # Printing the result of the algorithm
+        for (word, tag) in predictedWordsAndTags:
+            print word, tag,
+        print("\n")
+    print("\n=========================\n")
 
 def bigramHMMViterbiAlgorithm(sentence, allTags, emissionProbabilityDict, transitionProbabilityDict):
 
+    # Appending a '*' required for the initial bigram in the algorithm(base case)
     sentence = [ u'*'] + sentence
-    initialLength = len(sentence)
-    backtrackingDP = defaultdict(list)
-    dpDict = {}
-    dpDict[0, '*'] = 1
 
-    # print(allTags)
+    # Declaring the variables required
+    initialLength = len(sentence)
+    backtrackingDict = defaultdict(list)
+    dpDict = {}
+    tagsAssigned = []
+
+    # Initializing the base cases
+    dpDict[0, '*'] = 1
     for u in ([ u'*' ] + list(allTags)):
-        # for v in ([ u'*' ] + list(allTags)):
             if u != u'*':
                 dpDict[0, u] = 1
 
-
-    tagsAssigned = []
+    # Iterating through each word in the sentence from starting in terms of length
     for k in range(1, initialLength):
-        # print(" K : " + str(k))
-        # print(sentence[k])
+        # For all possibilities of (u, v) tags
         for v in allTags:
-                possibilities = []
-                for u in allTags:
-                    # if emissionProbabilityDict[sentence[k] + '|' + v] == 0:
-                    #     print("HELLO")
-                    #     return
-                    possibility = dpDict[k - 1 , u] * transitionProbabilityDict[(v, u)] * (emissionProbabilityDict[sentence[k] + '|' + v])
-                    # print(possibility)
-                    possibilities.append((possibility, u))
 
-                maxUVgivenK = max(possibilities, key = lambda element:element[0])
-                dpDict[k, v] = maxUVgivenK[0]
-                backtrackU = maxUVgivenK[1]
-                backtrackingDP[v] += [backtrackU]
-                if k == initialLength - 1:
-                    backtrackingDP[v] += [v]
-                # print((k, u, v),str(dpDict[k, u, v]))
-    # s = 0
-    # for key in dpDict:
-    #     s += 1
-    #     print(key, dpDict[key])
-    #
-    # print(s)
-    # backtrackingDP
+            # List for possibilities for getting maximum probability
+            possibilities = []
+
+            # For all possibilities of (u, v) tags
+            for u in allTags:
+
+                # Viterbi algorithm formulas
+                possibility = dpDict[k - 1 , u] * transitionProbabilityDict[(v, u)] * (emissionProbabilityDict[sentence[k] + '|' + v])
+
+                # Appending the possibility to the list of possibilities
+                possibilities.append((possibility, u))
+
+            # Getting the maximum probability from the list of possibilities, element[0] is the probability of the possibility
+            maxUVgivenK = max(possibilities, key = lambda element : element[0])
+
+            # Storing the answer in the list of probabilties
+            dpDict[k, v] = maxUVgivenK[0]
+
+            # Getting the element to be backtracked and storing it in the backtracking Dict
+            backtrackU = maxUVgivenK[1]
+            backtrackingDict[v] += [backtrackU]
+
+            # Additionally storing v of (u, v) tags in the last iteration
+            if k == initialLength - 1:
+                backtrackingDict[v] += [v]
+
+
+    # List for getting the possibilities of final iterations
     maxPossibilitiesList = []
-    s = 0
+
     for key in dpDict:
-        s += 1
-        if (initialLength - 1 in key) and dpDict[key]!=0:
-            # print(key[1], dpDict[key])
+
+        # Getting the possibilities of the final iterations and appending to maxPossibilitiesList
+        if (initialLength - 1 in key) and (dpDict[key] != 0):
             maxPossibilitiesList.append((key[1], dpDict[key]))
 
-    ans = max(maxPossibilitiesList, key = lambda element:element[1])
-    tagsAssigned = backtrackingDP[ans[0]][1:]
+    # Finally getting the answer of the viterbi alogrithm, element[1] is the probability of the final iteration possibilities
+    viterbiAnswer = max(maxPossibilitiesList, key = lambda element:element[1])
+
+    # Getting the tags associated with the viterbiAnswer by referring to the backtrackingDP
+    tagsAssigned = backtrackingDict[viterbiAnswer[0]][1:]
+
+    # Removing the '*' appended in the starting of algorithm, not required anymore
     sentence = sentence[1:]
-    # print(len(tagsAssigned), len(sentence))
-    # for i in range(len(sentence)):
-        # print sentence[i] + "  " + tagsAssigned[i] + "  " ,
-    # print(s)
+
+    # Returning the answer
     return zip(sentence, tagsAssigned)
 
 
 def calculateTransitionProbabilities(trainFile, Lambda):
 
+    # Declaring the variables and default dicts required
     transitionProbabilityDict = defaultdict(int)
     bigramTransitionCountDict = defaultdict(int)
     unigramTransitionCountDict = defaultdict(int)
-    s = 0
-    s1 = 0
+    numberOfWords = 0
+    numberOfSentences = 0
     allTags = set([])
     # Iterating through the lines of the input file
     for line in trainFile.readlines():
@@ -119,47 +152,58 @@ def calculateTransitionProbabilities(trainFile, Lambda):
             # Appending the tag to the list of tags
             tags.append(tag)
 
-        s += len(tags)
-        # print(tags)
+        # Incrementing the total number of words by the required amount
+        numberOfWords += len(tags)
 
         # If the line read is not a blank line
         if tags != []:
-            s1+=1
+
+            # Incrementing the total number of sentences by the required amount
+            numberOfSentences += 1
+
             # Getting the bigrams in the exact order by zipping through the tags
             unigramSentenceList = tags[:]
 
             # Getting the trigrams in the exact order by zipping through the tags
             bigramSentenceList = zip(tags, tags[1:])
 
+            # Incrementing the count of the unigrams as observed in the unigramSentenceList
             for unigram in unigramSentenceList:
                 unigramTransitionCountDict[unigram] += 1
 
-
+            # Incrementing the count of the unigrams as observed in the bigramSentenceList
             for bigram in bigramSentenceList:
                 bigramTransitionCountDict[bigram] += 1
 
     # Calculating the transition probabilities finally
+    # Iterating through the (u, v) bigrams
     for bigram in bigramTransitionCountDict:
+
+        # Initializing variables as required
         uUnigram = bigram[0]
         vState = bigram[1]
+
+        # Declaring the q(v | u) - transition probability
         keyVgivenU = (vState, uUnigram)
 
+        # Calculating the transition probability
         transitionProbability = (bigramTransitionCountDict[bigram] + Lambda ) / (unigramTransitionCountDict[uUnigram] + (Lambda * 22))
-        # print(transitionProbability)
+
+        # Storing the transition probability in the dict
         transitionProbabilityDict[keyVgivenU] = transitionProbability
 
-    # print("----------------------")
-    # s = 0
-    # for key in transitionProbabilityDict:
-    #     s+=1
-    #     print(key,transitionProbabilityDict[key])
-    print("Number of Words     : " + str(s))
-    print("Number of Sentences : " + str(s1))
+    # Printing the total number of words and sentences
+    print("Total number of Words     : " + str(numberOfWords))
+    print("Total number of Sentences : " + str(numberOfSentences))
+
+    # Returning probability dict and tags set
     return transitionProbabilityDict, allTags
 
 
 def calculateEmissionProbabilities(trainFile, Lambda):
 
+    # Declaring the variables and default dicts required
+    # 0.000000001 as offset for smoothing
     emissionProbabilityDict = defaultdict(lambda : 0.000000001)
     emissionCountDict = defaultdict(int)
     separateTagCountDict = defaultdict(int)
@@ -195,16 +239,21 @@ def calculateEmissionProbabilities(trainFile, Lambda):
                 # Calculating the number of times that tag appears in general
                 separateTagCountDict[tag] += 1
 
-
+    # Iterating through the tag and word in the keys of emissionCountDict
     for key in emissionCountDict:
 
+        # Getting the tag and word fromm the key
         tagAndWord = key.split('|')
         tag = tagAndWord[0]
         word = tagAndWord[1]
+
+        # Calculating the emission probability
         emissionProbability = (emissionCountDict[tag + '|' + word] + Lambda ) / (separateTagCountDict[tag] + (Lambda * 22))
-        # print(emissionProbability)
+
+        # Storing the emission probability in the dict
         emissionProbabilityDict[word + '|' + tag] = emissionProbability
 
+    # Returning probability dict
     return emissionProbabilityDict
 
 
