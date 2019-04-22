a31c191dce5e9eaa82ff9838b1506cc2003e3e95
Shashwat Kathuria <shashwatkathuria@yahoo.co.in>
2019-04-22 07:42:00 +0530

    Found best values of smoothing and applied them.

--- PartOfSpeechTagging/Viterbi-HMM/testAccuracyBigramViterbi.py
+++ PartOfSpeechTagging/Viterbi-HMM/testAccuracyBigramViterbi.py
@@ -8,33 +8,46 @@ from bigramViterbi import bigramHMMViterbiAlgorithm, calculateTransitionProbabil
 
 
 def main():
-    # Lambdas = [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
-        Lambda = 0.0
-    # for Lambda in Lambdas:
 
-        # Opening the input training file
-        trainFile = codecs.open("trainDataHindi.txt", mode = "r", encoding = "utf-8")
+    # CODE USED FOR CHECKING THE BEST VALUE OF LAMBDA AND OFFSET IS COMMENTED
+    # AND THE BEST VALUES ARE USED
 
-        # Getting the emission probabilities
-        emissionProbabilityDict = calculateEmissionProbabilities(trainFile, Lambda)
+    # Lambdas = [0.00, 0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09, 0.10, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
+    # Offsets = [1, 0.1, 0.01, 0.001, 0.0001, 0.00001, 0.000001, 0.0000001, 0.00000001, 0.000000001, 0.0000000001, 0.00000000001, 0.000000000001]
+    # offset = 0.000001
+    Offsets = [1e-14]
+    # for i in range(5):
+        # Offsets.append(offset)
+        # offset /= 100
+    Lambdas = [0.04]
+    for Lambda in Lambdas:
+        for offset in Offsets:
 
-        # Opening the input training file
-        trainFile = codecs.open("trainDataHindi.txt", mode = "r", encoding = "utf-8")
+            # Opening the input training file
+            trainFile = codecs.open("trainDataHindi.txt", mode = "r", encoding = "utf-8")
 
-        # Getting the transition probabilities and all the tags (states)
-        transitionProbabilityDict, allTags = calculateTransitionProbabilities(trainFile, Lambda)
+            # Getting the emission probabilities
+            emissionProbabilityDict = calculateEmissionProbabilities(trainFile, Lambda, offset)
 
-        # Opening the input test file
-        testFile = codecs.open("testDataHindi.txt", mode = "r", encoding = "utf-8")
+            # Opening the input training file
+            trainFile = codecs.open("trainDataHindi.txt", mode = "r", encoding = "utf-8")
 
-        # Calling the test function to get the accuracy of algorithm on the test data file
-        accuracy = testBigramHMMViterbiAlgorithm(testFile, allTags, emissionProbabilityDict, transitionProbabilityDict, Lambda)
+            # Getting the transition probabilities and all the tags (states)
+            transitionProbabilityDict, allTags = calculateTransitionProbabilities(trainFile, Lambda)
 
-        # Printing the accuracy result
-        print("============================")
-        print("ACCURACY : " + str(accuracy) + " %")
-        print("============================")
-        time.sleep(1)
+            # Opening the input test file
+            testFile = codecs.open("testDataHindi.txt", mode = "r", encoding = "utf-8")
+
+            # Calling the test function to get the accuracy of algorithm on the test data file
+            accuracy = testBigramHMMViterbiAlgorithm(testFile, allTags, emissionProbabilityDict, transitionProbabilityDict, Lambda)
+
+            # Printing the accuracy result
+            print("============================")
+            print("ACCURACY : " + str(accuracy) + " %" + " " + "Offset : " + str(offset) + " " + "Lambda : " + str(Lambda))
+            print("============================")
+            # print("Offset : " + str(offset))
+            # print("Lambda : " + str(Lambda))
+            time.sleep(5)
 
 def testBigramHMMViterbiAlgorithm(testFile, allTags, emissionProbabilityDict, transitionProbabilityDict, Lambda):
 
